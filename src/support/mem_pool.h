07032bbdce9047f6a783cba5063d84d6811608ef
Arsène Pérard-Gayot <arsene.perard@laposte.net>
2023-10-02 16:15:01 +0200

    Add memory pool

---
+++ src/support/mem_pool.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <stddef.h>
+
+struct mem_block;
+
+struct mem_pool {
+    struct mem_block* first;
+    struct mem_block* cur;
+};
+
+#define MEM_POOL_ALLOC(pool, T) mem_pool_alloc(&(pool), sizeof(T), alignof(T))
+
+struct mem_pool mem_pool_create(void);
+void mem_pool_destroy(struct mem_pool*);
+void mem_pool_reset(struct mem_pool*);
+void* mem_pool_alloc(struct mem_pool*, size_t size, size_t align);
