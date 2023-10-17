e80cf05c27d54b3e94e7e2042e9a0eaba173a80b
Arsène Pérard-Gayot <arsene.perard@laposte.net>
2023-10-17 22:45:37 +0200

    Rename `htable` -> `hash_table`

--- src/support/map.h
+++ src/support/map.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "htable.h"
+#include "hash_table.h"
 #include "visibility.h"
 
 #include <string.h>
@@ -9,14 +9,14 @@
 #define MAP_PREFIX map_very_long_prefix_
 
 #define MAP_FOREACH_KEY(key_ty, key, map) \
-    for (size_t MAP_PREFIX##i = 0; MAP_PREFIX##i < (map).htable.capacity; ++MAP_PREFIX##i) \
-        if (htable_is_bucket_occupied(&(map).htable, MAP_PREFIX##i)) \
+    for (size_t MAP_PREFIX##i = 0; MAP_PREFIX##i < (map).hash_table.capacity; ++MAP_PREFIX##i) \
+        if (hash_table_is_bucket_occupied(&(map).hash_table, MAP_PREFIX##i)) \
             for (bool MAP_PREFIX##once = true; MAP_PREFIX##once; MAP_PREFIX##once = false) \
-                for (key_ty const* key = &((key_ty const*)(map).htable.keys)[MAP_PREFIX##i]; MAP_PREFIX##once; MAP_PREFIX##once = false) \
+                for (key_ty const* key = &((key_ty const*)(map).hash_table.keys)[MAP_PREFIX##i]; MAP_PREFIX##once; MAP_PREFIX##once = false) \
 
 #define MAP_FOREACH(key_ty, key, val_ty, val, map) \
     MAP_FOREACH_KEY(key_ty, key, map) \
-        for (val_ty const* val = &((val_ty const*)(map).htable.vals)[MAP_PREFIX##i]; MAP_PREFIX##once; MAP_PREFIX##once = false) \
+        for (val_ty const* val = &((val_ty const*)(map).hash_table.vals)[MAP_PREFIX##i]; MAP_PREFIX##once; MAP_PREFIX##once = false) \
 
 #define MAP_DEFINE(name, key_ty, val_ty, hash, cmp, vis) \
     MAP_DECL(name, key_ty, val_ty, vis) \
@@ -24,7 +24,7 @@
 
 #define MAP_DECL(name, key_ty, val_ty, vis) \
     struct name { \
-        struct htable htable; \
+        struct hash_table hash_table; \
         size_t elem_count; \
     }; \
     [[nodiscard]] VISIBILITY(vis) struct name name##_create_with_capacity(size_t); \
@@ -41,23 +41,23 @@
     } \
     VISIBILITY(vis) struct name name##_create_with_capacity(size_t capacity) { \
         return (struct name) { \
-            .htable = htable_create(sizeof(key_ty), sizeof(val_ty), capacity) \
+            .hash_table = hash_table_create(sizeof(key_ty), sizeof(val_ty), capacity) \
         }; \
     } \
     VISIBILITY(vis) struct name name##_create(void) { \
         return name##_create_with_capacity(MAP_DEFAULT_CAPACITY); \
     } \
     VISIBILITY(vis) void name##_destroy(struct name* map) { \
-        htable_destroy(&map->htable); \
+        hash_table_destroy(&map->hash_table); \
     } \
     VISIBILITY(vis) void name##_clear(struct name* map) { \
-        htable_clear(&map->htable); \
+        hash_table_clear(&map->hash_table); \
         map->elem_count = 0; \
     } \
     VISIBILITY(vis) bool name##_insert(struct name* map, key_ty const* key, val_ty const* val) { \
-        if (htable_insert(&map->htable, key, val, sizeof(key_ty), sizeof(val_ty), hash(hash_init(), key), name##_cmp_wrapper)) { \
-            if (htable_needs_rehash(&map->htable, map->elem_count)) \
-                htable_grow(&map->htable, sizeof(key_ty), sizeof(val_ty)); \
+        if (hash_table_insert(&map->hash_table, key, val, sizeof(key_ty), sizeof(val_ty), hash(hash_init(), key), name##_cmp_wrapper)) { \
+            if (hash_table_needs_rehash(&map->hash_table, map->elem_count)) \
+                hash_table_grow(&map->hash_table, sizeof(key_ty), sizeof(val_ty)); \
             map->elem_count++; \
             return true; \
         } \
@@ -65,12 +65,12 @@
     } \
     VISIBILITY(vis) val_ty const* name##_find(const struct name* map, key_ty const* key) { \
         size_t idx; \
-        if (!htable_find(&map->htable, &idx, key, sizeof(key_ty), hash(hash_init(), key), name##_cmp_wrapper)) \
+        if (!hash_table_find(&map->hash_table, &idx, key, sizeof(key_ty), hash(hash_init(), key), name##_cmp_wrapper)) \
            return NULL; \
-        return ((val_ty const*)map->htable.vals) + idx; \
+        return ((val_ty const*)map->hash_table.vals) + idx; \
     } \
     VISIBILITY(vis) bool name##_remove(struct name* map, key_ty const* key) { \
-        if (htable_remove(&map->htable, key, sizeof(key_ty), sizeof(val_ty), hash(hash_init(), key), name##_cmp_wrapper)) { \
+        if (hash_table_remove(&map->hash_table, key, sizeof(key_ty), sizeof(val_ty), hash(hash_init(), key), name##_cmp_wrapper)) { \
             map->elem_count--; \
             return true; \
         } \
--- src/support/set.h
+++ src/support/set.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "htable.h"
+#include "hash_table.h"
 #include "visibility.h"
 
 #include <string.h>
@@ -9,10 +9,10 @@
 #define SET_PREFIX set_very_long_prefix_
 
 #define SET_FOREACH(elem_ty, elem, set) \
-    for (size_t SET_PREFIX##i = 0; SET_PREFIX##i < (set).htable.capacity; ++SET_PREFIX##i) \
-        if (htable_is_bucket_occupied(&(set).htable, SET_PREFIX##i)) \
+    for (size_t SET_PREFIX##i = 0; SET_PREFIX##i < (set).hash_table.capacity; ++SET_PREFIX##i) \
+        if (hash_table_is_bucket_occupied(&(set).hash_table, SET_PREFIX##i)) \
             for (bool SET_PREFIX##once = true; SET_PREFIX##once; SET_PREFIX##once = false) \
-                for (elem_ty const* elem = &((elem_ty const*)(set).htable.keys)[SET_PREFIX##i]; SET_PREFIX##once; SET_PREFIX##once = false) \
+                for (elem_ty const* elem = &((elem_ty const*)(set).hash_table.keys)[SET_PREFIX##i]; SET_PREFIX##once; SET_PREFIX##once = false) \
 
 #define SET_DEFINE(name, elem_ty, hash, cmp, vis) \
     SET_DECL(name, elem_ty, vis) \
@@ -20,7 +20,7 @@
 
 #define SET_DECL(name, elem_ty, vis) \
     struct name { \
-        struct htable htable; \
+        struct hash_table hash_table; \
         size_t elem_count; \
     }; \
     [[nodiscard]] VISIBILITY(vis) struct name name##_create_with_capacity(size_t); \
@@ -37,23 +37,23 @@
     } \
     VISIBILITY(vis) struct name name##_create_with_capacity(size_t capacity) { \
         return (struct name) { \
-            .htable = htable_create(sizeof(elem_ty), 0, capacity) \
+            .hash_table = hash_table_create(sizeof(elem_ty), 0, capacity) \
         }; \
     } \
     VISIBILITY(vis) struct name name##_create(void) { \
         return name##_create_with_capacity(SET_DEFAULT_CAPACITY); \
     } \
     VISIBILITY(vis) void name##_destroy(struct name* set) { \
-        htable_destroy(&set->htable); \
+        hash_table_destroy(&set->hash_table); \
     } \
     VISIBILITY(vis) void name##_clear(struct name* set) { \
-        htable_clear(&set->htable); \
+        hash_table_clear(&set->hash_table); \
         set->elem_count = 0; \
     } \
     VISIBILITY(vis) bool name##_insert(struct name* set, elem_ty const* elem) { \
-        if (htable_insert(&set->htable, elem, NULL, sizeof(elem_ty), 0, hash(hash_init(), elem), name##_cmp_wrapper)) { \
-            if (htable_needs_rehash(&set->htable, set->elem_count)) \
-                htable_grow(&set->htable, sizeof(elem_ty), 0); \
+        if (hash_table_insert(&set->hash_table, elem, NULL, sizeof(elem_ty), 0, hash(hash_init(), elem), name##_cmp_wrapper)) { \
+            if (hash_table_needs_rehash(&set->hash_table, set->elem_count)) \
+                hash_table_grow(&set->hash_table, sizeof(elem_ty), 0); \
             set->elem_count++; \
             return true; \
         } \
@@ -61,12 +61,12 @@
     } \
     VISIBILITY(vis) elem_ty const* name##_find(const struct name* set, elem_ty const* elem) { \
         size_t idx; \
-        if (!htable_find(&set->htable, &idx, elem, sizeof(elem_ty), hash(hash_init(), elem), name##_cmp_wrapper)) \
+        if (!hash_table_find(&set->hash_table, &idx, elem, sizeof(elem_ty), hash(hash_init(), elem), name##_cmp_wrapper)) \
            return NULL; \
-        return ((elem_ty const*)set->htable.keys) + idx; \
+        return ((elem_ty const*)set->hash_table.keys) + idx; \
     } \
     VISIBILITY(vis) bool name##_remove(struct name* set, elem_ty const* elem) { \
-        if (htable_remove(&set->htable, elem, sizeof(elem_ty), 0, hash(hash_init(), elem), name##_cmp_wrapper)) { \
+        if (hash_table_remove(&set->hash_table, elem, sizeof(elem_ty), 0, hash(hash_init(), elem), name##_cmp_wrapper)) { \
             set->elem_count--; \
             return true; \
         } \
