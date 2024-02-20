288ea8f5c4dc2ff627d1541a85a350814cbed913
Arsène Pérard-Gayot <arsene.perard@laposte.net>
2024-02-20 22:42:45 +0100

    Fix comments in code generation include file

--- include/fir/codegen.h
+++ include/fir/codegen.h
@@ -11,7 +11,7 @@
 /// Code generation is the last step to produce machine code. This is typically run after every
 /// optimization pass has been run.
 
-/// Enumeration containing the types of available target machines.
+/// Enumeration containing the types of code generators.
 enum fir_codegen_tag {
     FIR_CODEGEN_DUMMY,
     FIR_CODEGEN_LLVM
@@ -22,8 +22,9 @@ struct fir_mod;
 /// Abstract datatype representing a target machine to generate code for.
 struct fir_codegen;
 
-/// Creates a code generator from the given options specified as character strings.
-/// Available options depend on the target machine type.
+/// Creates a code generator from the given options specified as character strings. Available
+/// options depend on the code generator type. If the given code generator type is not available or
+/// cannot be created with the given set of options, this function returns `NULL`.
 FIR_SYMBOL struct fir_codegen* fir_codegen_create(
     enum fir_codegen_tag,
     const char** options,
@@ -34,7 +35,7 @@ FIR_SYMBOL void fir_codegen_destroy(struct fir_codegen*);
 
 /// Generates code for the given IR module and code generator object. The module may be modified by
 /// the code generator in order to simplify code generation, or to lower some unsupported constructs
-/// into supported ones. Returns true on success, false on failure.
+/// into supported ones. Returns `true` on success, `false` on failure.
 FIR_SYMBOL bool fir_codegen_run(
     struct fir_codegen*,
     struct fir_mod*,
